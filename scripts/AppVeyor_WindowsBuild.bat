e5b0068c58042819b851ab0e27f8e3a0f7d50bfb
tmp64 <tmp64@users.noreply.github.com>
2018-10-29 16:52:48 +0700

    Windows build script is almost ready...

--- scripts/AppVeyor_WindowsBuild.bat
+++ scripts/AppVeyor_WindowsBuild.bat
@@ -7,12 +7,12 @@ IF "%SERVER_BUILD%"=="0" (
 	MSBuild.exe BugfixedHL.sln /t:client /p:PlatformTarget=x86 /p:Configuration=Release /m
 	IF %errorlevel% neq 0 EXIT /b %errorlevel%
 	IF "%VGUI2_BUILD%"=="1" (
-		../scripts/AppVeyor_PackageClientVGUI2.bat
+		..\scripts\AppVeyor_PackageClientVGUI2.bat
 	) ELSE (
-		../scripts/AppVeyor_PackageClient.bat
+		..\scripts\AppVeyor_PackageClient.bat
 	)
 ) ELSE (
 	MSBuild.exe BugfixedHL.sln /t:hl /p:PlatformTarget=x86 /p:Configuration=Release /m
 	IF %errorlevel% neq 0 EXIT /b %errorlevel%
-	../scripts/AppVeyor_PackageServer.bat
+	..\scripts\AppVeyor_PackageServer.bat
 )
