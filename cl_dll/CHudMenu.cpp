tmp64/BugfixedHL

commit b371bf26a2e23b7d9466d2ecdf25100584b51154

[client] Colored menu support
Fixes https://github.com/alliedmodders/amxmodx/issues/553

TODO: Add support for color codes

cl_dll/CHudMenu.cpp
@@ -50,7 +50,6 @@ void CHudMenu :: InitHUDData( void )
 {
 	m_fMenuDisplayed = 0;
 	m_bitsValidSlots = 0;
-	m_iFlags &= ~HUD_ACTIVE;
 	Reset();
 }
 
@@ -64,6 +63,69 @@ int CHudMenu :: VidInit( void )
 {
 	return 1;
 }
+
+
+/*=================================
+  ParseEscapeToken
+
+  Interprets the given escape token (backslash followed by a letter). The
+  first character of the token must be a backslash.  The second character
+  specifies the operation to perform:
+
+   \w : White text (this is the default)
+   \d : Dim (gray) text
+   \y : Yellow text
+   \r : Red text
+   \R : Right-align (just for the remainder of the current line)
+=================================*/
+
+static int menu_r, menu_g, menu_b, menu_x, menu_ralign;
+
+static inline const char* ParseEscapeToken( const char* token )
+{
+	if ( *token != '\\' )
+		return token;
+
+	token++;
+
+	switch ( *token )
+	{
+	case '\0':
+		return token;
+
+	case 'w':
+		menu_r = 255;
+		menu_g = 255;
+		menu_b = 255;
+		break;
+
+	case 'd':
+		menu_r = 100;
+		menu_g = 100;
+		menu_b = 100;
+		break;
+
+	case 'y':
+		menu_r = 255;
+		menu_g = 210;
+		menu_b = 64;
+		break;
+
+	case 'r':
+		menu_r = 210;
+		menu_g = 24;
+		menu_b = 0;
+		break;
+
+	case 'R':
+		menu_x = ScreenWidth/2;
+		menu_ralign = TRUE;
+		break;
+	}
+
+	return ++token;
+}
+
 
 int CHudMenu :: Draw( float flTime )
 {
@@ -94,20 +156,51 @@ int CHudMenu :: Draw( float flTime )
 	}
 
 	// center it
-	int lineHeight = gHUD.m_scrinfo.iCharHeight + ADJUST_MENU;
-	int y = (ScreenHeight / 2) - ((nlc / 2) * lineHeight) - 40; // make sure it is above the say text
-	int x = 20;
+	int y = (ScreenHeight/2) - ((nlc/2)*12) - 40; // make sure it is above the say text
 
-	i = 0;
-	while ( i < MAX_MENU_STRING && g_szMenuString[i] != '\0' )
+	menu_r		= 255;
+	menu_g		= 255;
+	menu_b		= 255;
+	menu_x		= 20;
+	menu_ralign	 = FALSE;
+
+	const char* sptr = g_szMenuString;
+	
+	while ( *sptr != '\0' )
 	{
-		gHUD.DrawHudString( x, y, g_szMenuString + i, 255, 255, 255 );
-		y += lineHeight;
-
-		while ( i < MAX_MENU_STRING && g_szMenuString[i] != '\0' && g_szMenuString[i] != '\n' )
-			i++;
-		if ( g_szMenuString[i] == '\n' )
-			i++;
+		if ( *sptr == '\\' )
+		{
+			sptr = ParseEscapeToken( sptr );
+		}
+		else if ( *sptr == '\n' )
+		{
+			menu_ralign	 = FALSE;
+			menu_x		 = 20;
+			y			+= (12);
+			
+			sptr++;
+		}
+		else
+		{
+			char menubuf[ 80 ];
+			const char *ptr = sptr;
+			while ( *sptr != '\0' && *sptr != '\n' && *sptr != '\\')
+			{
+				sptr++;
+			}
+			strncpy( menubuf, ptr, min( ( sptr - ptr), (int)sizeof( menubuf ) ));
+			menubuf[ min( ( sptr - ptr), (int)(sizeof( menubuf )-1) ) ] = '\0';
+			
+			if ( menu_ralign )
+			{		
+				// IMPORTANT: Right-to-left rendered text does not parse escape tokens!
+				menu_x = gHUD.DrawHudStringReverse( menu_x, y, menubuf, menu_r, menu_g, menu_b );
+			}
+			else
+			{
+				menu_x = gHUD.DrawHudString( menu_x, y, menubuf, menu_r, menu_g, menu_b );
+			}
+		}
 	}
 	
 	return 1;

commit 6d32b046b0f4501bad27abc98b4ab3c9560764ed

[client] Fix phantom HUD menu when reconnecting with menu shown
Broken by b371bf26a2e23b7d9466d2ecdf25100584b51154

cl_dll/CHudMenu.cpp
@@ -50,6 +50,7 @@ void CHudMenu :: InitHUDData( void )
 {
 	m_fMenuDisplayed = 0;
 	m_bitsValidSlots = 0;
+	m_iFlags &= ~HUD_ACTIVE;
 	Reset();
 }
 