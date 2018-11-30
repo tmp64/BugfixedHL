d42b7064d50e23e2b8da4ef8459456b6d4a5243b
tmp64 <tmp64@users.noreply.github.com>
2018-11-30 19:26:59 +0700

    [client] Set g_bNewerBuild to true for non-Windows builds

--- cl_dll/memory_stub.cpp
+++ cl_dll/memory_stub.cpp
@@ -8,6 +8,14 @@
 #include "com_utils.h"
 #include "memory.h"
 
+// Inititalize with nullptr to trigger SIGSEGV when used
+void **g_EngineBuf = nullptr;
+int *g_EngineBufSize = nullptr;
+int *g_EngineReadPos = nullptr;
+UserMessage **g_pUserMessages = nullptr;
+// Any non-Windows build is new and uses SDL
+bool g_bNewerBuild = true;
+
 void Memory::OnLibraryInit()
 {
 }
