16ca8fe6605b5f78868ca734fbd721e1f3253f05
tmp64 <tmp32x2@gmail.com>
2020-01-28 16:28:43 +0700

    Server: Add ConVar class
    Source-like cvar definitions

---
+++ dlls/convar.cpp
@@ -0,0 +1,44 @@
+#include "extdll.h"
+#include "eiface.h"
+#include "util.h"
+#include "convar.h"
+
+ConVar *ConVar::m_pFirst = nullptr;
+
+ConVar::ConVar(const char *name, const char *defval, int flags)
+{
+	m_Cvar.name = (char *)name;
+	m_Cvar.string = (char *)defval;
+	m_Cvar.flags = flags;
+
+	m_pNext = m_pFirst;
+	m_pFirst = this;
+}
+
+float ConVar::Get()
+{
+	return m_Cvar.value;
+}
+
+void ConVar::Set(const char *val)
+{
+	CVAR_SET_STRING(m_Cvar.name, val);
+}
+
+void ConVar::Set(float val)
+{
+	CVAR_SET_FLOAT(m_Cvar.name, val);
+}
+
+ConVar::operator bool()
+{
+	return m_Cvar.value != 0;
+}
+
+void ConVar::RegisterAll()
+{
+	for (ConVar *pCvar = m_pFirst; pCvar; pCvar = pCvar->m_pNext)
+	{
+		CVAR_REGISTER(&pCvar->m_Cvar);
+	}
+}
---
+++ dlls/convar.h
@@ -0,0 +1,26 @@
+#ifndef CONVAR_H
+#define CONVAR_H
+#include <cvardef.h>
+
+class ConVar
+{
+public:
+	ConVar(const char *name, const char *defval, int flags = 0);
+	ConVar(const ConVar &) = delete;
+	ConVar &operator=(const ConVar &) = delete;
+
+	float Get();
+	void Set(const char *val);
+	void Set(float val);
+
+	explicit operator bool();
+
+	static void RegisterAll();
+
+private:
+	cvar_t m_Cvar;
+	ConVar *m_pNext = nullptr;
+	static ConVar *m_pFirst;
+};
+
+#endif
