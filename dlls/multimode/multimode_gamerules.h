c603a26c9da386ad68a759593fe14524844e2d0c
tmp64 <tmp32x2@gmail.com>
2020-03-06 20:14:21 +0700

    Multimode: Move CGameRules overrides to the end of the file

--- dlls/multimode/multimode_gamerules.h
+++ dlls/multimode/multimode_gamerules.h
@@ -38,56 +38,7 @@ public:
 
 	virtual const char *GetGameDescription();
 
-	void Think();
-	virtual void ClientDisconnected(edict_t *pClient);
-	virtual float FlPlayerFallDamage(CBasePlayer *pPlayer);
-	virtual BOOL  FPlayerCanTakeDamage(CBasePlayer *pPlayer, CBaseEntity *pAttacker);
-	virtual void PlayerSpawn(CBasePlayer *pPlayer);
-	virtual void PlayerThink(CBasePlayer *pPlayer);
-	virtual BOOL FPlayerCanRespawn(CBasePlayer *pPlayer);
-	virtual float FlPlayerSpawnTime(CBasePlayer *pPlayer);
-	virtual edict_t *GetPlayerSpawnSpot(CBasePlayer *pPlayer);
-	virtual int IPointsForKill(CBasePlayer *pAttacker, CBasePlayer *pKilled);
-	virtual void PlayerKilled(CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor);
-
-	// Weapon retrieval
-	virtual BOOL CanHavePlayerItem(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);// The player is touching an CBasePlayerItem, do I give it to him?
-	virtual void PlayerGotWeapon(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);// Called each time a player picks up a weapon from the ground
-
-	// Weapon spawn/respawn control
-	virtual int WeaponShouldRespawn(CBasePlayerItem *pWeapon);// should this weapon respawn?
-	virtual float FlWeaponRespawnTime(CBasePlayerItem *pWeapon);// when may this weapon respawn?
-	virtual float FlWeaponTryRespawn(CBasePlayerItem *pWeapon); // can i respawn now,  and if not, when should i try again?
-	virtual Vector VecWeaponRespawnSpot(CBasePlayerItem *pWeapon);// where in the world should this weapon respawn?
-	
-	// Item retrieval
-	virtual BOOL CanHaveItem(CBasePlayer *pPlayer, CItem *pItem);// is this player allowed to take this item?
-	virtual void PlayerGotItem(CBasePlayer *pPlayer, CItem *pItem);// call each time a player picks up an item (battery, healthkit, longjump)
-
-	// Item spawn/respawn control
-	virtual int ItemShouldRespawn(CItem *pItem);// Should this item respawn?
-	virtual float FlItemRespawnTime(CItem *pItem);// when may this item respawn?
-	virtual Vector VecItemRespawnSpot(CItem *pItem);// where in the world should this item respawn?
-
-	// Ammo retrieval
-	virtual BOOL CanHaveAmmo(CBasePlayer *pPlayer, const char *pszAmmoName, int iMaxCarry);// can this player take more of this ammo?
-	virtual void PlayerGotAmmo(CBasePlayer *pPlayer, char *szName, int iCount);// called each time a player picks up some ammo in the world
-
-	// Ammo spawn/respawn control
-	virtual int AmmoShouldRespawn(CBasePlayerAmmo *pAmmo);// should this ammo item respawn?
-	virtual float FlAmmoRespawnTime(CBasePlayerAmmo *pAmmo);// when should this ammo item respawn?
-	virtual Vector VecAmmoRespawnSpot(CBasePlayerAmmo *pAmmo);// where in the world should this ammo item respawn?
-																			// by default, everything spawns
-
-	// Healthcharger respawn control
-	virtual float FlHealthChargerRechargeTime();// how long until a depleted HealthCharger recharges itself?
-	virtual float FlHEVChargerRechargeTime();// how long until a depleted HealthCharger recharges itself?
-
-	// What happens to a dead player's weapons
-	virtual int DeadPlayerWeapons(CBasePlayer *pPlayer);// what do I do with a player's weapons when he's killed?
-
-	// What happens to a dead player's ammo	
-	virtual int DeadPlayerAmmo(CBasePlayer *pPlayer);// Do I drop ammo when the player dies? How much?
+	virtual void Think();
 
 	// After primary attack
 	void OnPrimaryAttack(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);
@@ -123,6 +74,58 @@ private:
 	void ResetTimerUpdate();
 
 	friend bool IsRunningMultimode(ModeID mode);
+
+public:
+	// CGameRules overrides
+	virtual void ClientDisconnected(edict_t *pClient);
+	virtual float FlPlayerFallDamage(CBasePlayer *pPlayer);
+	virtual BOOL  FPlayerCanTakeDamage(CBasePlayer *pPlayer, CBaseEntity *pAttacker);
+	virtual void PlayerSpawn(CBasePlayer *pPlayer);
+	virtual void PlayerThink(CBasePlayer *pPlayer);
+	virtual BOOL FPlayerCanRespawn(CBasePlayer *pPlayer);
+	virtual float FlPlayerSpawnTime(CBasePlayer *pPlayer);
+	virtual edict_t *GetPlayerSpawnSpot(CBasePlayer *pPlayer);
+	virtual int IPointsForKill(CBasePlayer *pAttacker, CBasePlayer *pKilled);
+	virtual void PlayerKilled(CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor);
+
+	// Weapon retrieval
+	virtual BOOL CanHavePlayerItem(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);// The player is touching an CBasePlayerItem, do I give it to him?
+	virtual void PlayerGotWeapon(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);// Called each time a player picks up a weapon from the ground
+
+	// Weapon spawn/respawn control
+	virtual int WeaponShouldRespawn(CBasePlayerItem *pWeapon);// should this weapon respawn?
+	virtual float FlWeaponRespawnTime(CBasePlayerItem *pWeapon);// when may this weapon respawn?
+	virtual float FlWeaponTryRespawn(CBasePlayerItem *pWeapon); // can i respawn now,  and if not, when should i try again?
+	virtual Vector VecWeaponRespawnSpot(CBasePlayerItem *pWeapon);// where in the world should this weapon respawn?
+
+	// Item retrieval
+	virtual BOOL CanHaveItem(CBasePlayer *pPlayer, CItem *pItem);// is this player allowed to take this item?
+	virtual void PlayerGotItem(CBasePlayer *pPlayer, CItem *pItem);// call each time a player picks up an item (battery, healthkit, longjump)
+
+	// Item spawn/respawn control
+	virtual int ItemShouldRespawn(CItem *pItem);// Should this item respawn?
+	virtual float FlItemRespawnTime(CItem *pItem);// when may this item respawn?
+	virtual Vector VecItemRespawnSpot(CItem *pItem);// where in the world should this item respawn?
+
+	// Ammo retrieval
+	virtual BOOL CanHaveAmmo(CBasePlayer *pPlayer, const char *pszAmmoName, int iMaxCarry);// can this player take more of this ammo?
+	virtual void PlayerGotAmmo(CBasePlayer *pPlayer, char *szName, int iCount);// called each time a player picks up some ammo in the world
+
+	// Ammo spawn/respawn control
+	virtual int AmmoShouldRespawn(CBasePlayerAmmo *pAmmo);// should this ammo item respawn?
+	virtual float FlAmmoRespawnTime(CBasePlayerAmmo *pAmmo);// when should this ammo item respawn?
+	virtual Vector VecAmmoRespawnSpot(CBasePlayerAmmo *pAmmo);// where in the world should this ammo item respawn?
+																			// by default, everything spawns
+
+	// Healthcharger respawn control
+	virtual float FlHealthChargerRechargeTime();// how long until a depleted HealthCharger recharges itself?
+	virtual float FlHEVChargerRechargeTime();// how long until a depleted HealthCharger recharges itself?
+
+	// What happens to a dead player's weapons
+	virtual int DeadPlayerWeapons(CBasePlayer *pPlayer);// what do I do with a player's weapons when he's killed?
+
+	// What happens to a dead player's ammo	
+	virtual int DeadPlayerAmmo(CBasePlayer *pPlayer);// Do I drop ammo when the player dies? How much?
 };
 
 #endif
